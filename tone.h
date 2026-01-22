f6ac3488e1dc9bd136655f80772b649cdf2ca420
SKarboy410 <saikarthik42006@gmail.com>
2026-01-22 21:28:07 +0530

    Add infinite tone support, busy flag and atomic interrupt handling

--- tone.h
+++ tone.h
@@ -5,7 +5,7 @@
 
 #define TONE_INFINITE UINT32_MAX
 
-void tone( uint16_t frequency, uint32_t duration, uint8_t pin);
+void tone(uint16_t frequency, uint32_t duration, uint8_t pin);
 void noTone(void); 
 
 
